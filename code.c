878f8486ee75264e8e84d5473964a46b83b58c16
username <you@example.com>
2023-07-28 15:06:36 +0530

    initial commit

---
+++ code.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    int a,b,c;
+    clrscr();
+    printf("enter a,b values");
+    scanf("%d%d",&a,&b);
+    c=a+b;
+    printf("the sum of two numbers are%d",c);
+    getch();
+}
